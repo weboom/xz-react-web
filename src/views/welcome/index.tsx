fbd3c2de2bb52500ad3bd26706ef6c43662a5a46
gaollard <gaollard@foxmail.com>
2019-09-29 15:57:28 +0800

    update

--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import apis from '../../apis';
+import './index.css';
 
 export default class Welcome extends React.Component {
   public state = {
@@ -7,15 +8,33 @@ export default class Welcome extends React.Component {
   }
 
   public componentDidMount () {
-    apis.getDemandList({}).then(res => {
-      console.log(res);
+    apis.getDemandList({}).then(ret => {
+      const res = ret as any;
+      if (res && res.success) {
+        this.setState({
+          list: res.data.list
+        })
+      }
     })
+  }
+
+  public renderList = () => {
+    const list: any[] = this.state.list;
+    return (
+      <div className="d-list">
+        {list.map((item, index) => {
+          return (
+            <div className="d-item" key={index}>{ item.title }</div>
+          )
+        })}
+      </div>
+    )
   }
 
   public render () {
     return (
-      <div>
-        Welcome
+      <div className="page-welcome">
+        {this.renderList()}
       </div>
     )
   }
