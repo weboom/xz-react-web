0f4c3b587a2529ce1f31d178959f1a835695f33b
gaollard <gaollard@foxmail.com>
2020-01-16 22:21:36 +0800

    update

--- src/views/search/index.tsx
+++ src/views/search/index.tsx
@@ -14,14 +14,30 @@ export default class extends React.Component<{}, MState> {
     key: ''
   }
 
-  doSearch = (key: string) => {
-    xzApi.getXzProductByKey({ key: key || this.state.key }).then((res: any) => {
-      if (res && res.success) {
-        this.setState({
-          list: res.data.list
-        })
-      }
-    })
+  doSearch = async (key: string) => {
+    const res: any = await xzApi.getXzProductByKey({ key: key || this.state.key })
+    if (res && res.success) {
+      this.setState({ list: res.data.list })
+    }
+  }
+
+  _handleKeyChange = (v: string) => {
+    this.setState({ key: v })
+    if (v) {
+      this.doSearch(v);
+    } else {
+      this.setState({ list: [] })
+    }    
+  }
+
+  _goBack = () => {
+    const props: any = this.props
+    props.history.goBack();
+  }
+
+  _toProduct = (product: any) => {
+    const props: any = this.props
+    props.history.push(`/item/${product.id}`);
   }
 
   render () {
@@ -29,29 +45,14 @@ export default class extends React.Component<{}, MState> {
       <div className="page-search">
         <div className="search-wrap">
           <div className="search-box">
-            <InputItem placeholder="输入关键词(比如：IPhone)" onChange={(v) => {
-              this.setState({
-                key: v
-              })
-              if (v) {
-                this.doSearch(v);
-              } else {
-                this.setState({
-                  list: []
-                })
-              }
-            }}/>
+            <InputItem placeholder="输入关键词(比如：IPhone)" onChange={this._handleKeyChange}/>
           </div>
-          <span className="btn-back" onClick={() => {
-          (this.props as any).history.goBack();
-        }}>取消</span>
+          <span className="btn-back" onClick={this._goBack}>取消</span>
         </div>
         {
           this.state.list.map((item: any, index) => {
             return (
-              <div className="g-item" key={index} onClick={() => {
-                (this.props as any).history.push(`/item/${item.id}`);
-              }}>
+              <div className="g-item" key={index} onClick={() => this._toProduct(item)}>
                 <div className="g-name">{item.title}</div>
               </div>
             )
