d59b91aed70b1298851ba86a722a2acc95277052
gaollard <gaollard@foxmail.com>
2020-01-22 18:19:43 +0800

    更新首页滚动条宽度的计算方式

--- src/components/xzCategoryList/index.tsx
+++ src/components/xzCategoryList/index.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import xzApi from '../../apis/xy';
+import './index.css'
 
 const classNames = require('classnames');
+
+let scrollerRef: any;
+let wrapperRef: any;
 
 interface Prop {
   categoryId?: string|number
@@ -10,25 +14,37 @@ interface Prop {
 
 export default class extends React.Component<Prop> {
   state = {
-    cMenu: []
+    cMenu: [],
+    width: 375
   }
 
-  public getXzCategorytList = () => {
+  getXzCategorytList = () => {
     xzApi.getXzCategorytList().then(res => {
-      this.setState({
-        cMenu: res.data.list
-      })
+      this.setState({ cMenu: res.data.list })
     })
   }
 
   componentDidMount () {
+    console.log(222)
     this.getXzCategorytList();
   }
 
-  public render = () => {
+  componentDidUpdate () {
+    if (scrollerRef && wrapperRef) {
+      const len = scrollerRef.children.length
+      let width = 0
+      for (let index = 0; index < len; index++) {
+        width += scrollerRef.children[index].offsetWidth
+      }
+      wrapperRef.style.width = (width + 10) + 'px'
+    }
+  }
+
+  render = () => {
     const cMenu: any[] = this.state.cMenu;
     return (
-      <div className="cate-list">
+      <div className="cate-list" ref={ref => wrapperRef = ref}>
+        <div className="scroller" ref={ref => scrollerRef = ref}>
         {
           cMenu.map(item => {
             const cls = classNames('cate-item', {
@@ -41,6 +57,7 @@ export default class extends React.Component<Prop> {
             )
           })
         }
+        </div>
       </div>
     )
   }
